5e8af2f8b6f9a77b728cd5144c8c7d96ab9aac20
i-bilge <iismailbilge@gmail.com>
2022-05-12 22:32:33 +0200

    first lesson of JS is added.

---
+++ week2/Thursday/app.js
@@ -0,0 +1,9 @@
+document.write("this text comes from externel js file. And its not under the h2, because JS link is in the header")
+
+// it is for comment
+/*it
+is
+for
+commenting
+more 
+lines*/
---
+++ week2/Thursday/variables.js
@@ -0,0 +1,26 @@
+document.write("Variables JS");
+
+var x = 5;
+var y = 7;
+var z = 25;
+
+document.write("the variable x: ", x);
+
+/*
+NAMING RULES
+- understendable
+- workable
+- without spaes
+- first character must be letter
+- reserved words cannot be used
+- it s case sensitiv
+*/
+
+// addition "let"and "const":
+/*
+there are 3 scopes as "global", "block" and "funktion"
+var define a variable in global scope and u can use it from ecery wher
+let and const defining variable in block scope and its just usable in {
+const is but unchangable
+}
+*/
