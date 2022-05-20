05a8baedb95c043ecd11edbf580f134ad5b6ca5a
i-bilge <iismailbilge@gmail.com>
2022-05-20 15:41:39 +0200

    Review with do-while statement is added.

--- week003/Thursday/appReview.js
+++ week003/Thursday/appReview.js
@@ -1,21 +1,34 @@
 //HOME WORK
-let counter1 = 0;
-let sum1 = 0;
 
-while (counter1 < 10) {
-    sum1 += counter1;
-    console.log("counter1 :", counter1);
-    counter1++;
-}
-console.log("sum1 :", sum1);
-//------------------------------------------------------
+// //while
+// let counter1 = 0;
+// let sum1 = 0;
 
-let counter2 = 9;
-let sum2 = 0;
+// while (counter1 < 10) {
+//     sum1 += counter1;
+//     console.log("counter1 :", counter1);
+//     counter1++;
+// }
+// console.log("sum1 :", sum1);
+// //------------------------------------------------------
 
-while (counter2 > 0) {
-    sum2 += counter2;
-    console.log("counter2 :", counter2);
-    counter2--;
-}
-console.log("sum2 :", sum2);
+// let counter2 = 9;
+// let sum2 = 0;
+
+// while (counter2 > 0) {
+//     sum2 += counter2;
+//     console.log("counter2 :", counter2);
+//     counter2--;
+// }
+// console.log("sum2 :", sum2);
+
+//do while
+let counter = 9;
+let sum = 0;
+
+do {
+    sum += counter;
+    console.log("counter :", counter);
+    counter--;
+} while (counter > 0);
+console.log("sum :", sum);
