8d65ab1f1fab8111806ad11b75be727ff1c16e65
i-bilge <iismailbilge@gmail.com>
2022-05-20 12:22:13 +0200

    Review of 19 May 2022 is added.

---
+++ week003/Thursday/appReview.js
@@ -0,0 +1,21 @@
+//HOME WORK
+let counter1 = 0;
+let sum1 = 0;
+
+while (counter1 < 10) {
+    sum1 += counter1;
+    console.log("counter1 :", counter1);
+    counter1++;
+}
+console.log("sum1 :", sum1);
+//------------------------------------------------------
+
+let counter2 = 9;
+let sum2 = 0;
+
+while (counter2 > 0) {
+    sum2 += counter2;
+    console.log("counter2 :", counter2);
+    counter2--;
+}
+console.log("sum2 :", sum2);
