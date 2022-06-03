7c7e9197f3dc14d23812aa201f10819a56f129bc
i-bilge <iismailbilge@gmail.com>
2022-06-03 22:29:18 +0200

    calculator, score-keeper and timer examples added.

--- week005/Thursday/appPingPong.js
+++ week005/Thursday/appPingPong.js
@@ -1,13 +1,22 @@
 let score1 = 0;
-let winningScore = 5;
-
+let score2 = 0;
+let winningScore = 5; //document.getElementById("winningNumber");
 const btnFirstPlayer = document.getElementById("btnFirstPlayer");
 const firstPlayer = document.getElementById("firstPlayer");
+const btnSecondPlayer = document.getElementById("btnSecondPlayer");
+const secondPlayer = document.getElementById("secondPlayer");
 
 btnFirstPlayer.addEventListener("click", increaseScore1);
-
-
 function increaseScore1(){
     if(score1 < winningScore)  score1++;
     firstPlayer.innerText = score1;
 }
+
+btnSecondPlayer.addEventListener("click", increaseScore2);
+function increaseScore2() {
+    if (score2 < winningScore) score2++;
+    secondPlayer.innerText = score2;
+}
+
+if (score1 == winningScore) alert("First Player is winner!");
+else if (score2 == winningScore) alert("Second Player is winner!");
