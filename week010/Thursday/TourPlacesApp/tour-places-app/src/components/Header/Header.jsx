cea9bcbd3135614584d33c88adfb9d8f37315dcb
i-bilge <iismailbilge@gmail.com>
2022-07-07 22:32:27 +0200

    Tour App studied.

---
+++ week010/Thursday/TourPlacesApp/tour-places-app/src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import Card from "../main/Card";
+import "./Header.scss";
+
+const HeaderDaniel = () => {
+  return (
+    <header className="header">
+      POPULAR TOUR PLACES
+    </header>
+  );
+};
+
+export default HeaderDaniel;
---
+++ week010/Thursday/TourPlacesApp/tour-places-app/src/components/main/Card.jsx
@@ -0,0 +1,15 @@
+const Card = ({ city }) => {
+  return (
+    <div className="cards">
+      <div className="title">
+        <h1>{city.title}</h1>
+      </div>
+      <img src={city.image} alt="City view" />
+      <div className="card-over">
+        <p>{city.desc}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
