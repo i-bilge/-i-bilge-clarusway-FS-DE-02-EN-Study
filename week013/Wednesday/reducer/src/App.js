29833730607dd9d635e99eeb8e1a91bd098885a8
i-bilge <iismailbilge@gmail.com>
2022-07-28 10:34:41 +0200

    useReducer lesson is repeated

--- week013/Wednesday/reducer/src/App.js
+++ week013/Wednesday/reducer/src/App.js
@@ -7,6 +7,56 @@ import ComponentA from "./components/ComponentA";
 import DataFetchingOne from "./components/DataFetchingOne";
 import DataFetchingTwo from "./components/DataFetchingTwo";
 
+//STEPS: !-useReducer(<reducer>, <initialState>) / 1-import { useReducer } from "react";
+// 2-const initialTodos = [
+//   {
+//     id: 1,
+//     title: "Todo 1",
+//     complete: false,
+//   },
+//   {
+//     id: 2,
+//     title: "Todo 2",
+//     complete: false,
+//   },
+// ];
+// 3-const reducer = (state, action) => {
+//   switch (action.type) {
+//     case "COMPLETE":
+//       return state.map((todo) => {
+//         if (todo.id === action.id) {
+//           return { ...todo, complete: !todo.complete };
+//         } else {
+//           return todo;
+//         }
+//       });
+//     default:
+//       return state;
+//   }
+// };
+// 4-function Todos() {
+//   const [todos, dispatch] = useReducer(reducer, initialTodos);
+
+//   const handleComplete = (todo) => {
+//     dispatch({ type: "COMPLETE", id: todo.id });
+//   };
+// LAST-  return (
+//   <>
+//     {todos.map((todo) => (
+//       <div key={todo.id}>
+//         <label>
+//           <input
+//             type="checkbox"
+//             checked={todo.complete}
+//             onChange={() => handleComplete(todo)}
+//           />
+//           {todo.title}
+//         </label>
+//       </div>
+//     ))}
+//   </>
+// );
+//-----------------------------------------------------------------------------------------
 const initialState = 0;
 
 const reducer = (state, action) => {
