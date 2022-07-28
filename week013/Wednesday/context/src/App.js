75cb7419d9f2b3c4b337c4dc70ab322a088bf94a
i-bilge <iismailbilge@gmail.com>
2022-07-28 10:10:17 +0200

    useContext class is repeated

--- week013/Wednesday/context/src/App.js
+++ week013/Wednesday/context/src/App.js
@@ -2,6 +2,29 @@ import { useState, createContext, useEffect } from "react";
 import "./App.css";
 import ComponentC from "./components/ComponentC";
 
+//STEPS: 1-import { useState, createContext, useContext } / 2-const UserContext = createContext(); 
+// 3- function Component1() {
+//   const [user, setUser] = useState("Jesse Hall");
+
+//   return (
+//     <UserContext.Provider value={user}>
+//       <h1>{`Hello ${user}!`}</h1>
+//       <Component2 user={user} />
+//     </UserContext.Provider>
+//   );
+// }
+// 4- function Component5() {
+//   const user = useContext(UserContext);
+
+//   return (
+//     <>
+//       <h1>Component 5</h1>
+//       <h2>{`Hello ${user} again!`}</h2>
+//     </>
+//   );
+// }
+//----------------------------Abstract of the topic----------------------------------------------------------------
+
 export const CounterContext = createContext();
 export const TodoContext = createContext();
 
