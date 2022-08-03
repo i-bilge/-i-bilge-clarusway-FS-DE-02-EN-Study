7417f8eb3b919891ff56c89b036cadb33aff7a06
i-bilge <iismailbilge@gmail.com>
2022-08-03 10:26:04 +0200

    router js reviewed

--- week014/Monday/movie-app/src/router/Router.js
+++ week014/Monday/movie-app/src/router/Router.js
@@ -1,24 +1,21 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import Login from "../pages/Login";
+import Main from "../pages/Main";
+import { MovieDetail } from "../pages/MovieDetail";
+import Register from "../pages/Register";
 
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
-import Navbar from "../components/Navbar"
-import Main from "../pages/Main"
-import Login from "../pages/Login"
-import Register from "../pages/Register"
-import {MovieDetail} from "../pages/MovieDetail"
-
-function AppRouter() {
+const AppRouter = () => {
   return (
     <Router>
-        <Navbar/>
-        <Routes>
-            <Route path="/" element={<Main />}/>
-            <Route path="/login" element={<Login />}/>
-            <Route path="/register" element={<Register />}/>
-            <Route path="/details" element={<MovieDetail />}/>
-
-        </Routes>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/details/:id" element={<MovieDetail />} />
+      </Routes>
     </Router>
-  )
-}
-
-export default AppRouter
+  );
+};
+export default AppRouter;
