5460aeab5b9df4518229eaff7d4d72812ca2775b
i-bilge <iismailbilge@gmail.com>
2022-08-05 20:10:22 +0200

    useMemo is studied and useRef app created

---
+++ week014/Friday/hooks-lesson/src/AppUseMemo.js
@@ -0,0 +1,32 @@
+import { useMemo, useState } from 'react';
+import './App.css';
+
+function App() {
+  const [number,setNumber] = useState(0)
+  const [dark, setDark] = useState(false)
+  const doubleNumber = useMemo(()=>{
+    return slowFunction(number)
+  }, [number])
+
+  const themeStyles = {
+    backgroundColor: dark? 'black' : 'white',
+    color: dark? 'white' : 'black'
+  }
+
+  return (
+    <>
+      <input type="number" value={number} onChange={(e)=>setNumber(parseInt(e.target.value))}></input>
+      <button onClick={()=>setDark(prevDark => !prevDark)}>Change Theme</button>
+      <div style={themeStyles}>{doubleNumber}</div>
+    </>
+  );
+}
+
+function slowFunction(num){
+  console.log("calling slow function")
+  for(let i = 0; i < 1000000000; i++){
+  }
+  return num*2;
+}
+
+export default App;
