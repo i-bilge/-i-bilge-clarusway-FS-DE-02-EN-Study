b99ba8d265d12b37415e5cc468eadda9da424ef2
i-bilge <iismailbilge@gmail.com>
2022-09-02 22:21:00 +0200

    Students app is created in home

--- week017(Bckend)/Thursday/home/models.py
+++ week017(Bckend)/Thursday/home/models.py
@@ -1,3 +1,13 @@
+from pyexpat import model
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    first_name = models.CharField(max_length=30)
+    lastt_name = models.CharField(max_length=30)
+    number = models.IntegerField()
+    about = models.TextField(null=True, blank=True)
+    register_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.first_name
