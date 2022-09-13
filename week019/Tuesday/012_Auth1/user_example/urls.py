34c76b93c00825c6750c72e03c758f1adec49f65
i-bilge <iismailbilge@gmail.com>
2022-09-13 21:24:37 +0200

    reset password page is added.

--- week019/Tuesday/012_Auth1/user_example/urls.py
+++ week019/Tuesday/012_Auth1/user_example/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from .views import home
-from django.contrib.auth.views import PasswordChangeView
+from django.contrib.auth.views import PasswordChangeView, PasswordResetView
 
 
 urlpatterns = [
     path('', home, name='home'),
     path('change_password/', PasswordChangeView.as_view(template_name='registration/change_password.html'), name='change_password'),
+    path('reset_password/', PasswordResetView.as_view(template_name='registration/reset_password.html'), name='reset_password'),
 ]
