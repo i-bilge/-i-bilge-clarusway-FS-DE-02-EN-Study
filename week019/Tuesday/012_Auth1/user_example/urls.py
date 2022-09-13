f6ae513e3da2395d3039b0a669d0f79209e79f14
i-bilge <iismailbilge@gmail.com>
2022-09-13 22:09:07 +0200

    Registration is completed.

--- week019/Tuesday/012_Auth1/user_example/urls.py
+++ week019/Tuesday/012_Auth1/user_example/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home
+from .views import home, register
 from django.contrib.auth.views import PasswordChangeView, PasswordResetView
 
 
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', home, name='home'),
     path('change_password/', PasswordChangeView.as_view(template_name='registration/change_password.html'), name='change_password'),
     path('reset_password/', PasswordResetView.as_view(template_name='registration/reset_password.html'), name='reset_password'),
+    path('register/', register, name='register')
 ]
--- week019/Tuesday/012_Auth1/user_example/views.py
+++ week019/Tuesday/012_Auth1/user_example/views.py
@@ -1,6 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+
+from django.contrib.auth import login, authenticate
 
 
 # Create your views here.
 def home(request):
     return render(request, 'user_example/home.html')
+
+def register(request):
+    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            
+            return redirect('home')
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'registration/registration.html', context)
