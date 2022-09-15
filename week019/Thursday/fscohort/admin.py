9e536dc377c7a8186ed762763a500f51402334e5
i-bilge <iismailbilge@gmail.com>
2022-09-15 20:13:12 +0200

    admin panel in UI is changed

--- week019/Thursday/fscohort/admin.py
+++ week019/Thursday/fscohort/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 from .models import Student
 
 # Register your models here.
-admin.site.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display =  ("name", "number", "is_active")
+
+admin.site.register(Student, StudentAdmin)
