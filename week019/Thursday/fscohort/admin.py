d206d44caff4a391403e1bbd3a801c27849791a9
i-bilge <iismailbilge@gmail.com>
2022-09-15 20:17:39 +0200

    filter popup is added to admin page

--- week019/Thursday/fscohort/admin.py
+++ week019/Thursday/fscohort/admin.py
@@ -3,6 +3,7 @@ from .models import Student
 
 # Register your models here.
 class StudentAdmin(admin.ModelAdmin):
-    list_display =  ("name", "number", "is_active")
+    list_display =  ("name", "number", "is_active","register_date")
+    list_filter = ("is_active", "update_date")
 
 admin.site.register(Student, StudentAdmin)
