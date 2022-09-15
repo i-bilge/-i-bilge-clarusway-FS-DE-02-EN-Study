452bfb7cf8b1f2631649b0e4768f0d0a4a248dc5
i-bilge <iismailbilge@gmail.com>
2022-09-15 19:59:00 +0200

    Student data added with fake model

--- week019/Thursday/fscohort/models.py
+++ week019/Thursday/fscohort/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=50)
+    number = models.IntegerField()
+    about = models.TextField()
+    is_active = models.BooleanField(default=False)
+    register_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateField(auto_now=True)
+    slug = models.SlugField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
