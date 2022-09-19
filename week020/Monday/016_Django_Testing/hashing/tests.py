a6869a79d65ae7ceff5e4af79db59aeabd14fa8c
i-bilge <iismailbilge@gmail.com>
2022-09-19 20:30:36 +0200

    first unit test is added to check home page

--- week020/Monday/016_Django_Testing/hashing/tests.py
+++ week020/Monday/016_Django_Testing/hashing/tests.py
@@ -1,15 +1,22 @@
 from selenium import webdriver
 from django.test import TestCase
+import time
 
-class FunctionalTestCase():
+class FunctionalTestCase(TestCase):
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_homepage(self):
+        self.browser.get("http://127.0.0.1:8000/")
+        time.sleep(1)
+        assert self.browser.page_source.find("Enter hash here")
+
+
+class UnitTestCase(TestCase):
     pass
 
-class UnitTestCase():
-    pass
 
-browser = webdriver.Chrome()
 
-browser.get("http://127.0.0.1:8000/")
-
-assert browser.page_source.find("Enter hash here")
 
