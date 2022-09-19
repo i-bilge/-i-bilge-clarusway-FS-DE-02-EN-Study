8df8370d51ccd7f1f6dcc17599e7149875908064
i-bilge <iismailbilge@gmail.com>
2022-09-19 21:24:15 +0200

    unit test added for testing templete of home

--- week020/Monday/016_Django_Testing/hashing/tests.py
+++ week020/Monday/016_Django_Testing/hashing/tests.py
@@ -2,27 +2,30 @@ from selenium import webdriver
 from django.test import TestCase
 import time
 
-class FunctionalTestCase(TestCase):
-    def setUp(self):
-        self.browser = webdriver.Chrome()
-    def tearDown(self):
-        self.browser.quit()
+# class FunctionalTestCase(TestCase):
+#     def setUp(self):
+#         self.browser = webdriver.Chrome()
+#     def tearDown(self):
+#         self.browser.quit()
 
-    def test_homepage(self):
-        self.browser.get("http://127.0.0.1:8000/")
-        time.sleep(1)
-        assert self.browser.page_source.find("Enter hash here")
+#     def test_homepage(self):
+#         self.browser.get("http://127.0.0.1:8000/")
+#         time.sleep(1)
+#         assert self.browser.page_source.find("Enter hash here")
 
-    def test_hash_of_hello(self):
-        self.browser.get("http://127.0.0.1:8000/")
-        text =  self.browser.find_element_by_id('id_text')
-        text.send_keys('hello')
-        self.browser.find_element_by_name('submit').click()
-        self.assertInHTML('2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824', self.browser.page_source)
+#     def test_hash_of_hello(self):
+#         self.browser.get("http://127.0.0.1:8000/")
+#         text =  self.browser.find_element_by_id('id_text')
+#         text.send_keys('hello')
+#         self.browser.find_element_by_name('submit').click()
+#         self.assertInHTML('2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824', self.browser.page_source)
 
 
 class UnitTestCase(TestCase):
-    pass
+
+    def test_home_templete_rendering(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response ,'hashing/home.html')
 
 
 
