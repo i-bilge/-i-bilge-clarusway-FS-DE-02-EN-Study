07101a935cb5accae17765f73921859cbb1f0ba5
i-bilge <iismailbilge@gmail.com>
2022-09-19 21:49:41 +0200

    hello output is tested

--- week020/Monday/016_Django_Testing/hashing/tests.py
+++ week020/Monday/016_Django_Testing/hashing/tests.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from django.test import TestCase
 import time
 from .forms import HashForm
+from .models import Hash
 
 # class FunctionalTestCase(TestCase):
 #     def setUp(self):
@@ -36,4 +37,11 @@ class UnitTestCase(TestCase):
     def test_hash_func_works(self):
         hash = hashlib.sha256('hello'.encode('utf-8')).hexdigest()
         self.assertEqual('2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824', hash)
+
+    def test_hash_object(self):
+        hash = Hash.objects.create(text='hello', hash='2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824')
+
+        pulled_hash = Hash.objects.get(hash='2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824')
+
+        self.assertEqual(hash.text, pulled_hash.text)
 
