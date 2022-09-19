98036e9ce2de4938c9c4a1098da4ee852cbf65bf
i-bilge <iismailbilge@gmail.com>
2022-09-19 21:14:01 +0200

    functional test for hash searching to hello added.

--- week020/Monday/016_Django_Testing/hashing/tests.py
+++ week020/Monday/016_Django_Testing/hashing/tests.py
@@ -13,6 +13,13 @@ class FunctionalTestCase(TestCase):
         time.sleep(1)
         assert self.browser.page_source.find("Enter hash here")
 
+    def test_hash_of_hello(self):
+        self.browser.get("http://127.0.0.1:8000/")
+        text =  self.browser.find_element_by_id('id_text')
+        text.send_keys('hello')
+        self.browser.find_element_by_name('submit').click()
+        self.assertInHTML('2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824', self.browser.page_source)
+
 
 class UnitTestCase(TestCase):
     pass
