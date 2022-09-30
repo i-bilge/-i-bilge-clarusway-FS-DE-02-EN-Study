b84401de9d5404ff50437f7f129d1172c251456d
i-bilge <iismailbilge@gmail.com>
2022-09-30 19:34:18 +0200

    venv and requirementstxt is created

--- week021/01_Blog_App/users/forms.py
+++ week021/01_Blog_App/users/forms.py
@@ -7,12 +7,30 @@ from .models import Profile
 from django.contrib.auth.forms import UserCreationForm
 
 class RegisterationForm(UserCreationForm):
+    email = forms.EmailField()
 
     class Meta:
         model=User
         fields=("username", "email", "first_name", "last_name")
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+
+        if email == "":
+            raise forms.ValidationError("Email is requird!")
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("Emails should be unique, that email is registred.")
+        return email
+
 class ProfileForm(forms.ModelForm):
 
     class Meta:
         model=Profile
         fields=("bio", "image")
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+    
+    class Meta:
+        model=User
+        fields = ("username", "email", "first_name", "last_name")
--- week021/01_Blog_App/users/views.py
+++ week021/01_Blog_App/users/views.py
@@ -1,9 +1,10 @@
 from multiprocessing import context
 from re import I
 from django.shortcuts import render, redirect
-from .forms import RegisterationForm, ProfileForm
+from .forms import RegisterationForm, ProfileForm, UserUpdateForm
 from django.contrib import messages
 from .models import Profile
+
 # from .signals import *
 
 # Create your views here.
@@ -30,7 +31,7 @@ def register(request):
     return render(request, 'users/register.html', context)
 
 def profile(request):
-    u_form = RegisterationForm(request.POST or None, instance = request.user)
+    u_form = UserUpdateForm(request.POST or None, instance = request.user)
     p_form = ProfileForm(request.POST or None, instance=request.user.profile, files=request.FILES)
 
     if u_form.is_valid() and p_form.is_valid():
